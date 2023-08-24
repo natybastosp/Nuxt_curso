e64a4de13ed57ef027290abb267b0ec3d041f9a0
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2023-08-24 10:07:18 -0300

    home/page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,31 @@
-teste teste
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+      corrupti possimus ipsam quae nostrum hic dicta officiis corporis commodi
+      nobis nesciunt exercitationem, alias veniam quos repellat ad, cum delectus
+      ea!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+      assumenda. Omnis hic dicta ab vitae in animi earum perferendis dolore
+      provident ipsum sequi tempore, aliquid velit optio vero laudantium quas.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin-bottom: 20px;
+  font-size: 18px;
+  line-height: 30px;
+}
+</style>
