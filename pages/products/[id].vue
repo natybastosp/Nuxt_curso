2da5d1aebd72edf8a1d64cb2620fb704ed924b9f
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2023-08-28 10:10:09 -0300

    fetching data

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,23 +1,16 @@
 <template>
   <div>
-    <p>Products details for {{ id }}</p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-      obcaecati laborum ut ipsam odio voluptatum voluptatem? Itaque quos
-      provident nostrum incidunt error ratione est, sunt quis ullam? Ea, sunt
-      doloremque.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo repellat
-      veritatis cupiditate dolor fuga reprehenderit aliquam optio dolorum animi
-      dolorem iure nostrum unde, provident eveniet voluptatibus distinctio!
-      Nihil, modi dicta?
-    </p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/` + id;
+
+const { data: product } = await useFetch(uri, { key: id });
 
 definePageMeta({
   layout: "products",
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <p>Products</p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,8 @@
 definePageMeta({
   layout: "products",
 });
+
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
 
 <style scoped></style>
