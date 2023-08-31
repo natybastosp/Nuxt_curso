f834ed00b96b6176c6d7315984085bbba006af95
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2023-08-31 09:54:35 -0300

    teste

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,20 +1,17 @@
 <template>
   <div>
-    <p>{{ product.title }}</p>
-    <p>{{ product.price }}</p>
-    <p>{{ product.id }}</p>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
-const uri = `https://fakestoreapi.com/products/` + id;
+const uri = "https://fakestoreapi.com/products/" + id;
 
+//  fetch the products
 const { data: product } = await useFetch(uri, { key: id });
 
 definePageMeta({
   layout: "products",
 });
 </script>
-
-<style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,18 +2,17 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+//  fetch the products
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 definePageMeta({
   layout: "products",
 });
-
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
-
-<style scoped></style>
